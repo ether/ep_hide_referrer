193cb4285e51d3348a088189cddbf49c1c47ec44
John McLear <john@mclear.co.uk>
2013-06-06 23:48:47 +0100

    some basics

---
+++ static/js/index.js
@@ -0,0 +1,25 @@
+exports.aceGetFilterStack = function(name, context){
+  return [
+    context.linestylefilter.getRegexpFilter(
+      new RegExp("http.+((\.[pP][nN][gG])|(\.[jJ][pP][gG])|(\.[gG][iI][fF])|(\.[jJ][pP][eE][gG])|(\.[bB][mM][pP]))", "g"), 'image')
+  ];
+}
+
+
+exports.aceCreateDomLine = function(name, args){
+  if (args.cls.indexOf('image') > -1) { // If it's an image
+    var src;
+    cls = args.cls.replace(/(^| )image:(\S+)/g, function(x0, space, image) {
+      src = image;
+      return space + "image image_" + image;
+    });
+
+   return [{
+     cls: cls,
+     extraOpenTags: '<img src="' + src + '" style="max-width:100%" /><br/>',
+     extraCloseTags:''
+   }];
+  }
+}
+
+
