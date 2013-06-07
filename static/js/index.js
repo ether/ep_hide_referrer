cc528ba8abf7f31b0dac8046614085a9872ad1b3
The Dod <unclezzzen@gmail.com>
2013-06-08 06:22:16 +0700

    Remove incorrect text from popup
    "most browsers" was an exaggeration :)

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 exports.postAceInit = function(hook, context){
   $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").on('click', "a", function (e){
     prompt(
-      "Copy this link and paste it to the adress bar of some other browser tab\n(to prevent the browser from leaking this pad's confidential URL as a \"referer\").\nCtrl-C + Ctrl-T + Ctrl-V + Enter should do the trick on most browsers ;)",
+      "Copy this link and paste it to the adress bar of some other browser tab (to prevent the browser from leaking this pad's confidential URL as a \"referer\").",
       e.currentTarget.innerHTML
     );
     e.preventDefault();
