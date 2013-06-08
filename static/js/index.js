70c670a83c22f5f75a2642baead4fbf313154509
John McLear <john@mclear.co.uk>
2013-06-08 13:31:35 +0100

    use correct URL

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,9 @@
 exports.postAceInit = function(hook, context){
   $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").on('click', "a", function (e){
+    console.log(e);
     prompt(
       "Copy this link and paste it to the adress bar of some other browser tab (to prevent the browser from leaking this pad's confidential URL as a \"referer\").",
-      e.currentTarget.innerHTML
+      e.originalEvent.srcElement.href
     );
     e.preventDefault();
     return false;
