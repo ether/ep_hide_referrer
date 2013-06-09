4336a75d8ce26536156fad6398f468f0538bbd8f
The Dod <unclezzzen@gmail.com>
2013-06-09 16:41:27 +0700

    paranoid-friendly extra-click redir
    User gets to see a link and has to click it (or copy/paste for
    the extra paranoid). Makes it clear that the plugin *is* installed :)
    
    Also added postTimesliderInit ;)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 exports.registerRoute = function (hook_name, args, callback) {
   args.app.get('/redirect', function(req, res) {
-    res.send("<script type='text/javascript'> function readCookie(name) {  var nameEQ = name + '=';  var ca = document.cookie.split(';');  for(var i=0;i < ca.length;i++) {  var c = ca[i]; while (c.charAt(0)==' ') c = c.substring(1,c.length); if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length); } return null; } window.location = readCookie('url'); </script>");
+    res.send(
+"<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n  <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">\n  <title>External link</title>\n  <script type=\"text/javascript\">\n    window.redir_onload = function() {\n      var urlhash = location.hash.substr(1);\n      if (urlhash.search(':')<0) {\n        urlhash = unescape(urlhash);\n      }\n      if (urlhash.search(/^https?:\\/\\//) !== 0) {\n        urlhash = '';\n      }\n      var the_content = document.getElementById('content');\n      if (urlhash) {\n        the_content.innerHTML =\n          '<h4>Click to visit this exteral link</h4><h3><a href=\"'+urlhash+'\">'+urlhash+'</a></h3>';\n      } else {\n        the_content.innerHTML = '<h4>Nothing to see here. Move along</h4>';\n      }\n    };\n  </script>\n</head>\n<body onload=\"redir_onload()\">\n<div id=\"content\" style=\"text-align:center\">For the next trick, we'll need a volunteer with javascript enabled :)</div>\n</body>\n</html>"
+    );
   });
 }
--- static/js/index.js
+++ static/js/index.js
@@ -1,19 +1,15 @@
 exports.postAceInit = function(hook, context){
   $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").on('click', "a", function (e){
-    createCookie("url", e.originalEvent.srcElement.href);
-    window.open('/redirect');
+    window.open('../redirect#'+escape(e.originalEvent.target.href));
     e.preventDefault();
     return false;
   });
 }
 
-function createCookie(name,value,days) {
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime()+(days*24*60*60*1000));
-        var expires = "; expires="+date.toGMTString();
-    }
-   else var expires = "";
-   document.cookie = name+"="+value+expires+"; path=/";
+exports.postTimesliderInit = function(hook, context){
+  $('#padcontent').on('click', "a", function (e){
+    window.open('../../redirect#'+escape(e.originalEvent.target.href));
+    e.preventDefault();
+    return false;
+  });
 }
-
