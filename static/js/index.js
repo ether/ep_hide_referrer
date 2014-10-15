63cedd1d173d465f55871623ffd1aa722ca2cb1d
John McLear <john@mclear.co.uk>
2014-10-15 19:39:19 +0100

    redirect on chat msg too

--- static/js/index.js
+++ static/js/index.js
@@ -4,6 +4,12 @@ exports.postAceInit = function(hook, context){
     e.preventDefault();
     return false;
   });
+
+  $('#chattext > p').on('click', "a", function (e){
+    window.open('../redirect#'+escape(e.currentTarget.href));
+    e.preventDefault();
+    return false;
+  });
 }
 
 exports.postTimesliderInit = function(hook, context){
