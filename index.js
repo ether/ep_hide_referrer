d2e983c36872b2346e6b0b5199be7e4a71e95d42
nashe <thomas@chauchefoin.fr>
2018-04-08 20:15:27 +0200

    Remove useless logging call

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var eejs = require('ep_etherpad-lite/node/eejs');
 
 exports.registerRoute = function (hook_name, args, callback) {
   args.app.get('/redirect', function(req, res) {
-    console.log(__dirname);
     res.send(eejs.require(__dirname + "/templates/redirect.html"));
   });
 }
