08093ef810c0a68d8d27c6d449844184bdf03959
guido <guido@dis.tur.bio>
2018-04-18 17:44:37 -0300

    context menu support
    This fix the behavior on the right click -> open in new tab action.
    
    On the pad content the value of the href is changed everytime there is a right
    click, as any value we set there gets overwritten by etherpad. On the chats the
    situation is different, the href value is permanent so we add a class to know that
    we changed the value of the href attribute and not add "../redirect#" again.
    
    These changes are needed for Firefox ESR and Tor Browser at least.

--- static/js/index.js
+++ static/js/index.js
@@ -5,11 +5,28 @@ exports.postAceInit = function(hook, context){
     return false;
   });
 
+  $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").on('contextmenu', "a", function (e){
+    $(this).attr('href','../redirect#'+escape(e.currentTarget.href));
+  });
+
   $('#chattext').on('click', "a", function (e){
-    window.open('../redirect#'+escape(e.currentTarget.href));
+    if ( $(this).hasClass('no-referrer') ) {
+        window.open(e.currentTarget.href);
+    } else {
+        window.open('../redirect#'+escape(e.currentTarget.href));
+    }
     e.preventDefault();
     return false;
   });
+
+  $('#chattext').on('contextmenu', "a", function (e){
+    if ( !$(this).hasClass('no-referrer') ) {
+       $(this).attr('rel','noreferrer');
+       $(this).addClass('no-referrer');
+       $(this).attr('href','../redirect#'+escape(e.currentTarget.href));
+    }
+  });
+
 }
 
 exports.postTimesliderInit = function(hook, context){
@@ -18,4 +35,9 @@ exports.postTimesliderInit = function(hook, context){
     e.preventDefault();
     return false;
   });
+
+  $('#padcontent').on('contextmenu', 'a', function (e){
+    $(this).attr('rel','noreferrer');
+  });
+
 }
