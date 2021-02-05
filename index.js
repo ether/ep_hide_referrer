e99d5bf10fd7e3f4ed20408f9a927bbfce484c1f
John McLear <john@mclear.co.uk>
2021-02-05 17:20:24 +0000

    lint and various fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const path = require('path');
+'use strict';
+
 const eejs = require('ep_etherpad-lite/node/eejs');
 
-exports.registerRoute = function (hook_name, args, callback) {
+exports.registerRoute = (hookName, args, callback) => {
   args.app.get('/redirect', (req, res) => {
-    console.log(__dirname);
     res.send(eejs.require(`${__dirname}/templates/redirect.html`));
   });
 };
--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,45 @@
-exports.postAceInit = function (hook, context) {
-  $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').on('click', 'a', (e) => {
+'use strict';
+
+exports.postAceInit = (hook, context) => {
+  const $inner =
+      $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody');
+  $inner.on('click', 'a', (e) => {
     window.open(`../redirect#${escape(e.currentTarget.href)}`);
     e.preventDefault();
     return false;
   });
 
-  $('#chattext > p').on('click', 'a', (e) => {
-    window.open(`../redirect#${escape(e.currentTarget.href)}`);
+  $inner.on('contextmenu', 'a', function (e) {
+    $(this).attr('href', `../redirect#${escape(e.currentTarget.href)}`);
+  });
+
+  $('#chattext').on('click', 'a', function (e) {
+    if ($(this).hasClass('no-referrer')) {
+      window.open(e.currentTarget.href);
+    } else {
+      window.open(`../redirect#${escape(e.currentTarget.href)}`);
+    }
     e.preventDefault();
     return false;
+  });
+
+  $('#chattext').on('contextmenu', 'a', function (e) {
+    if (!$(this).hasClass('no-referrer')) {
+      $(this).attr('rel', 'noreferrer');
+      $(this).addClass('no-referrer');
+      $(this).attr('href', `../redirect#${escape(e.currentTarget.href)}`);
+    }
   });
 };
 
-exports.postTimesliderInit = function (hook, context) {
+exports.postTimesliderInit = (hook, context) => {
   $('#padcontent').on('click', 'a', (e) => {
     window.open(`../../redirect#${escape(e.currentTarget.href)}`);
     e.preventDefault();
     return false;
+  });
+
+  $('#padcontent').on('contextmenu', 'a', function (e) {
+    $(this).attr('rel', 'noreferrer');
   });
 };
